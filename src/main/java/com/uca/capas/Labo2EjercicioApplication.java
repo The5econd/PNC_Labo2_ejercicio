d9ef394524b7257ef561eedd1b2f7e41a85a34e3
The5econd <00017917@uca.edu.sv>
2020-03-30 20:25:39 -0600

    first commit, terminado

---
+++ src/main/java/com/uca/capas/Labo2EjercicioApplication.java
@@ -0,0 +1,13 @@
+package com.uca.capas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Labo2EjercicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Labo2EjercicioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uca/capas/Labo2EjercicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uca.capas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Labo2EjercicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
